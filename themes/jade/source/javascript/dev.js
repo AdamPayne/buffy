eb78522e01989928e7f42ec639a7b40c03c8601d
clstl <reamles@gmail.com>
2016-04-23 22:09:19 +0300

    end of branch

--- themes/jade/source/javascript/dev.js
+++ themes/jade/source/javascript/dev.js
@@ -1,7 +1,8 @@
-window.onload = function() {
-
-  var grid = new Hashgrid({
-    numberOfGrids: 1
-  });
-
-};
+$('#cont').pagination({
+  dataSource: page.posts.toArray(),
+  callback: function (data, pagination) {
+    // template method of yourself
+    var html = template(data);
+    dataContainer.html(html);
+  }
+})
